36ab85867a61e3c8b42abb05d1f72a85f163dbe7
kwanimaa <akousakwanimaaaa@gmail.com>
2026-03-06 23:13:03 +0000

    streak

--- trail6.java
+++ trail6.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 public class trail6 {
 
     public static void main (String[] args){
-        
+        Scanner input =new Scanner(System.in);
+        int hoursworked;
+        System.out.println("HOW MANY HOURS ARE YOU WORKING??");
+        hoursworked=input.nextInt();
+
+        //40hrs=regular, more than 40hrs=overtime
+        if(hoursworked<=40)
+        {
+            System.out.println("YOU WORKED AS REGULAR");
+            countreg
+        }
+        else
+        {
+            System.out.println("YOU WORKED OVERTIME TODAY");
+        }
+
+
+      
+        input.close();
+
     }
     
 }
